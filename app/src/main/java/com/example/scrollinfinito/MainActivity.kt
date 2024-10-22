6847447121b1e01c4fe23ce6b87976c50c65d4ce
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-10-22 09:33:01 +0200

    añadido el control para añadir tareas

--- app/src/main/java/com/example/scrollinfinito/MainActivity.kt
+++ app/src/main/java/com/example/scrollinfinito/MainActivity.kt
@@ -3,26 +3,35 @@ package com.example.scrollinfinito
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnAniadir:Button
     lateinit var txtTarea:EditText
     lateinit var rvTarea:RecyclerView
-    var task= mutableListOf<String>()
+
+    lateinit var adapter:TaskAdapter
+
+    var tasks= mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        aniadirTarea()
     }
 
     private fun aniadirTarea(){
         initView()
         initListener()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        rvTarea.layoutManager=LinearLayoutManager(this)
+        adapter=TaskAdapter(tasks)
+        rvTarea.adapter=adapter
     }
 
     private fun initListener() {
@@ -31,7 +40,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun addTask() {
         val taskToAdd=txtTarea.text.toString()
-        task.add(taskToAdd)
+        tasks.add(taskToAdd)
     }
 
     private fun initView() {
