bb931a1269ba625651fcc34e4bf8302a2e11ce1d
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-10-22 09:04:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/scrollinfinito/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.scrollinfinito
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnAniadir:Button
+    lateinit var txtTarea:EditText
+    lateinit var rvTarea:RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    private fun aniadirTarea(){
+        initView()
+        initListener()
+    }
+
+    private fun initListener() {
+        btnAniadir.setOnClickListener{addTask()}
+    }
+
+    private fun addTask() {
+        val taskToAdd=txtTarea.text.toString()
+    }
+
+    private fun initView() {
+        btnAniadir=findViewById(R.id.btnAniadir)
+        txtTarea=findViewById(R.id.txtTarea)
+        rvTarea=findViewById(R.id.rvTarea)
+    }
+}
